a922a3b3ee7cb013f39cf8ad841978270f49766c
Aaron Nielsen <apn@didactex.com>
2020-11-23 12:00:36 -0500

    added ability to read from file

--- py/minka_remote.py
+++ py/minka_remote.py
@@ -2,12 +2,15 @@
 run the minka-aire ceiling fan with a configurable cycle
 
 Usage:
-  minka_remote.py [ON] [OFF] [FILE]
+  minka_remote.py [ON] [OFF]
+  minka_remote.py --file=FILE
 
 Arguments:
   ON   time on in secconds [600]
   OFF  time off in seconds [3000]
-  FILE  file with fan configuration settings
+
+Options:
+  --file=FILE  file with fan configuration settings
 
 """
 from __future__ import print_function
@@ -18,6 +21,8 @@ import time
 from mar import MinkaAireRemote
 
 from docopt import docopt
+
+import sys
 
 if __name__=='__main__':
 
@@ -35,15 +40,16 @@ if __name__=='__main__':
     else:
         time_off = int(time_off)
 
-    if not args['FILE'] is None:
-        setfile = args['FILE']
+    if not args['--file'] is None:
+        setfile = args['--file']
+        print('using configfile {0}'.format(setfile))
         with open(setfile) as f:
-            fan_speed = f.readline()
-            time_on = int(f.readline())
-            time_off = int(f.readline())
+            fan_speed = f.readline().strip()
+            time_on = int(f.readline().strip())
+            time_off = int(f.readline().strip())
 
     print('on for {0} seconds, off for {1} seconds'.format(time_on,time_off))
-    
+    sys.exit()
     mr = MinkaAireRemote()
 
     while True:
