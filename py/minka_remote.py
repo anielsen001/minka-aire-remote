495002023754f4291fe5af5e12d93875dc6aa243
Aaron Nielsen <apn@didactex.com>
2020-11-23 12:15:21 -0500

    created separate functions

--- py/minka_remote.py
+++ py/minka_remote.py
@@ -23,7 +23,40 @@ from mar import MinkaAireRemote
 from docopt import docopt
 
 import sys
+USEGPIO = False
 
+def main(time_on, time_off, fan_speed):
+    
+    mr = MinkaAireRemote()
+    
+    while True:
+        # set fan on low for 10 minutes
+        print( str(datetime.datetime.now()) + ' : Setting fan to low' )
+        mr.fan_low()
+        time.sleep( time_on )
+
+        # set fan off for 50 minutes
+        print( str(datetime.datetime.now()) + ' : Setting fan to off' )
+        mr.fan_off()
+        time.sleep( time_off )
+
+        # repeat, etc.
+
+def main_file(filename):
+    """
+    run the main with a filename argument
+    """
+    time_on, time_off, fan_speed = parse_file(filename)
+    main(time_on, time_off, fan_speed)
+
+def parse_file(filename):
+    with open(filename) as f:
+        fan_speed = f.readline().strip()
+        time_on = int(f.readline().strip())
+        time_off = int(f.readline().strip())
+    return time_on, time_off, fan_speed
+
+    
 if __name__=='__main__':
 
     args = docopt(__doc__)
@@ -43,24 +76,15 @@ if __name__=='__main__':
     if not args['--file'] is None:
         setfile = args['--file']
         print('using configfile {0}'.format(setfile))
-        with open(setfile) as f:
-            fan_speed = f.readline().strip()
-            time_on = int(f.readline().strip())
-            time_off = int(f.readline().strip())
+        time_on, time_off, fan_speed = parse_file(setfile)
 
     print('on for {0} seconds, off for {1} seconds'.format(time_on,time_off))
-    sys.exit()
-    mr = MinkaAireRemote()
 
-    while True:
-        # set fan on low for 10 minutes
-        print( str(datetime.datetime.now()) + ' : Setting fan to low' )
-        mr.fan_low()
-        time.sleep( time_on )
+    if not USEGPIO:
+        sys.exit()
 
-        # set fan off for 50 minutes
-        print( str(datetime.datetime.now()) + ' : Setting fan to off' )
-        mr.fan_off()
-        time.sleep( time_off )
+    main(time_on, time_off, fan_speed)
+    
+    
 
-        # repeat, etc.
+
